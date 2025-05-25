58bc78d7cc42b3f7d277370eb80f7eee8017c4b6
bengo <171782+gobengo@users.noreply.github.com>
2025-05-24 21:19:45 -0700

    README.md

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # was
 
 Wallet Attached Storage Command Line Interface
+
+## Usage
+
+`wasup --help` output:
+
+```shell
+# wasup
+
+A tool that uploads to Wallet Attached Storage
+
+Usage:
+  wasup <path/to/file> <space-url> [--identity ~/.ssh/space-controller-key]
+  wasup -h | --help
+
+Options:
+  -h --help     Show this screen.
+  -i --identity Path to the Ed25519 SSH key to use for authentication.
+                Generate an identity with `ssh-keygen -t ed25519`.
+```
+
+## Examples
+
+```shell
+wasup ./README.md $WAS/space/$SPACE/README.md --identity ~/.ssh/id_ed25519_was_test
+```
+
+### Upload a space, linkset, ACL, and HTML home page
+
+See [./examples/acl-simple.sh](./examples/acl-simple.sh).
---
+++ examples/acl-simple.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -e # exit on error
+
+IDENTITY="$HOME/.ssh/id_ed25519_wasup_examples"
+SPACE_UUID="$(uuidgen | tr "[:upper:]" "[:lower:]")" # generate a random UUID
+WAS=https://storage.bengo.is
+SPACE_PATH=/space/$SPACE_UUID
+SPACE="$WAS$SPACE_PATH"
+
+# Space
+wasup --content-type application/json /dev/stdin "$SPACE" -i "$IDENTITY" <<EOF
+{
+  "controller": "did:key:z6MkmoJbuoR9TSY4evRTLAtw6nN44RtCtqFhxdv72PhEa91X",
+  "link": "$SPACE_PATH/links/"
+}
+EOF
+
+# /links/ - Links including link to ACL
+wasup --content-type application/linkset+json /dev/stdin "$SPACE"/links/ -i "$IDENTITY" <<EOF
+{
+  "linkset": [                                                             
+    {                                                         
+      "anchor": "$SPACE_PATH/",
+      "acl": [
+        {            
+          "href": "$SPACE_PATH/acl"
+        }
+      ]
+    }
+  ]
+}
+EOF
+
+# /acl - ACL
+wasup --content-type application/ld+json /dev/stdin "$SPACE"/acl -i "$IDENTITY" <<EOF
+{
+  "authorization": [
+    {
+      "agentClass": "http://xmlns.com/foaf/0.1/Agent",         
+      "accessTo": [
+        "$SPACE_PATH/"
+      ],                                                           
+      "mode": [
+        "Read"
+      ],
+      "@type": [
+        "http://www.w3.org/ns/auth/acl#Authorization"
+      ]
+    }             
+  ],
+  "@context": [
+    "http://www.w3.org/ns/auth/acl#"
+  ]
+}
+EOF
+
+# / - HTML Homepage
+wasup --content-type text/html /dev/stdin "$SPACE"/ -i "$IDENTITY" <<EOF
+<!doctype html>
+<h1>wasup/examples/acl-simple.sh</h1>
+<p>This is a simple example of something uploaded from wasup</p>
+EOF
