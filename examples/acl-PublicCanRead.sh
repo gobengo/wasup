f79e572f1b21ab028bbb415e4a661f96249af5ac
bengo <171782+gobengo@users.noreply.github.com>
2025-05-28 11:01:16 -0700

    add examples/acl-PublicCanRead.sh

---
+++ examples/acl-PublicCanRead.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -e # exit on error
+
+IDENTITY="$HOME/.ssh/id_ed25519_wasup_examples"
+SPACE_UUID="${SPACE_UUID:-"$(uuidgen | tr "[:upper:]" "[:lower:]")"}" # generate a random UUID
+WAS="${WAS:-https://storage.bengo.is}"
+SPACE_PATH=/space/$SPACE_UUID
+SPACE="$WAS$SPACE_PATH"
+
+# Space
+wasup --content-type application/json /dev/stdin "$SPACE" -i "$IDENTITY" <<EOF
+{
+  "controller": "did:key:z6MkmoJbuoR9TSY4evRTLAtw6nN44RtCtqFhxdv72PhEa91X",
+  "link": "$SPACE_PATH/links"
+}
+EOF
+
+# /links/ - Links including link to ACL
+wasup --content-type application/linkset+json /dev/stdin "$SPACE"/links -i "$IDENTITY" <<EOF
+{
+  "linkset": [                                                             
+    {                                                         
+      "anchor": "$SPACE_PATH/",
+      "acl": [
+        {            
+          "href": "$SPACE_PATH/acl"
+        }
+      ]
+    }
+  ]
+}
+EOF
+
+# /acl - ACL
+wasup --content-type application/json /dev/stdin "$SPACE"/acl -i "$IDENTITY" <<EOF
+{
+  "type": "PublicCanRead"
+}
+EOF
+
+# / - HTML Homepage
+wasup --content-type text/html /dev/stdin "$SPACE"/ -i "$IDENTITY" <<EOF
+<!doctype html>
+<h1>wasup/examples/acl-PublicCanRead.sh</h1>
+<p>This is an example of a resource covered by an ACL of type PubliCanRead.</p>
+EOF
+
+curl $SPACE/
